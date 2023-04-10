6199da4a26acde873165e399a21f3e4bd4acfabf
PedroMateusCunha <29759070+PedroMateusCunha@users.noreply.github.com>
2023-04-10 01:51:48 -0300

    [NEW] First commit

---
+++ botao_emergencia/botao_emergencia.py
@@ -0,0 +1,14 @@
+class BotaoDeEmergencia:
+    def __init__(self):
+        self.ligado = False
+
+    def ligar(self):
+        self.ligado = True
+        return {"message": "Botão de emergência ligado com sucesso"}
+
+    def desligar(self):
+        self.ligado = False
+        return {"message": "Botão de emergência desligado com sucesso"}
+    
+    def get_status(self):
+        return {"ligado": self.ligado}
